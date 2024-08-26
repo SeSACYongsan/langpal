e6330e31890e800c431a45aa15cfc85717e413dc
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-08-26 16:52:29 +0900

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:langpal/screens/initialization_screen.dart';
+import 'package:langpal/screens/profile_setting_screen.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: InitializationScreen(),
+      home: ProfileSettingScreen(),
     ),
   );
 }
---
+++ lib/screens/profile_setting_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class ProfileSettingScreen extends StatelessWidget {
+  const ProfileSettingScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/images/earth.png",
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: Colors.white70,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            offset: Offset(5, 5),
+                            color: Colors.black12,
+                          )
+                        ]),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.all(100),
+                            elevation: 5,
+                            shape: const CircleBorder(),
+                          ),
+                          onPressed: () {},
+                          child: const Text("프로필 사진"),
+                        ),
+                        const SizedBox(height: 30),
+                        const Text(
+                          "닉네임 입력",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 200,
+                          child: TextField(),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(15),
+                      elevation: 10,
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "다음으로",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
