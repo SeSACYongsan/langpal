22fb49dccd5b7e30bcc3dab05bea9133bfd0584a
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-08-26 16:03:08 +0900

    Update sign_in_screen.dart

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -92,7 +92,8 @@ class SignInScreen extends StatelessWidget {
                           ),
                         ),
                       ),
-                    )
+                    ),
+                    const SizedBox(height: 20),
                   ],
                 ),
               ],
