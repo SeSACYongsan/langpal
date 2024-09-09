a1750a1c31fef21e638aa5ff486c183e3736ad08
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-09-10 07:25:47 +0900

    Update

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -37,59 +37,55 @@ class MainScreen extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30),
-        child: ListView.builder(
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(50),
-                  child: Image.asset("assets/images/profile.png", width: 70),
-                ),
-                const SizedBox(width: 20),
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                        boxShadow: const [
-                          BoxShadow(
-                            spreadRadius: 5,
-                            blurRadius: 5,
-                            offset: Offset(5, 5),
-                            color: Colors.black12,
-                          ),
-                        ],
-                      ),
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(dummyQuestions[index].owner),
-                              Text(
-                                  "${dummyQuestions[index].point.toString()}pt"),
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          Text(dummyQuestions[index].content)
-                        ],
-                      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.asset("assets/images/profile.png", width: 70),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          spreadRadius: 5,
+                          blurRadius: 5,
+                          offset: Offset(5, 5),
+                          color: Colors.black12,
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(dummyQuestions[index].owner),
+                            Text("${dummyQuestions[index].point.toString()}pt"),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Text(dummyQuestions[index].content)
+                      ],
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          itemCount: dummyQuestions.length,
         ),
+        itemCount: dummyQuestions.length,
       ),
       backgroundColor: Colors.white,
     );
