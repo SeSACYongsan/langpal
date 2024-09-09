a84438b7b1d96f60a8f6b9d40374c5a97ffe0c9a
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-09-09 15:27:06 +0900

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:langpal/screens/subscription_detail_screen.dart';
+import 'package:langpal/screens/sign_in_screen.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: SubscriptionDetailScreen(),
+      home: SignInScreen(),
     ),
   );
 }
