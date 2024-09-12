af2b04d09ca83d75de8e582a0970ce4e4b9c063f
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-09-13 08:55:14 +0900

    Update new_question_screen.dart

--- lib/screens/new_question_screen.dart
+++ lib/screens/new_question_screen.dart
@@ -39,92 +39,95 @@ class _NewQuestionScreenState extends ConsumerState<NewQuestionScreen> {
           },
         ),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              DropdownMenu(
-                onSelected: (value) {
-                  ref
-                      .read(questionTypeProvider.notifier)
-                      .setQuestionType(value!);
-                },
-                expandedInsets: EdgeInsets.zero,
-                controller: questionTypeTextEditingController,
-                dropdownMenuEntries: QuestionType.values.map((questionType) {
-                  return DropdownMenuEntry(
-                      value: questionType, label: questionType.toKoreanName());
-                }).toList(),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1, color: Colors.black),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: TextField(
-                  controller: newQuestionTextEditingController,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "질문을 입력하세요",
-                  ),
-                  keyboardType: TextInputType.text,
-                  maxLines: 8,
-                  onChanged: (value) {
-                    ref.read(newQuestionProvider.notifier).setString(value);
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                DropdownMenu(
+                  onSelected: (value) {
+                    ref
+                        .read(questionTypeProvider.notifier)
+                        .setQuestionType(value!);
                   },
+                  expandedInsets: EdgeInsets.zero,
+                  controller: questionTypeTextEditingController,
+                  dropdownMenuEntries: QuestionType.values.map((questionType) {
+                    return DropdownMenuEntry(
+                        value: questionType,
+                        label: questionType.toKoreanName());
+                  }).toList(),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  Text(
-                    "0",
-                    style: Theme.of(context).textTheme.titleMedium,
+                const SizedBox(height: 20),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.black),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  Expanded(
-                    child: Slider(
-                      value: ref.watch(pointSliderProvider),
-                      min: 0,
-                      max: 100,
-                      thumbColor: Colors.blue,
-                      activeColor: Colors.blue,
-                      onChanged: (newValue) {
-                        ref
-                            .read(pointSliderProvider.notifier)
-                            .setPoint(newValue);
-                      },
+                  child: TextField(
+                    controller: newQuestionTextEditingController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "질문을 입력하세요",
                     ),
+                    keyboardType: TextInputType.text,
+                    maxLines: 5,
+                    onChanged: (value) {
+                      ref.read(newQuestionProvider.notifier).setString(value);
+                    },
                   ),
-                  Text(
-                    "100",
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                ],
-              ),
-              const Spacer(),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(15),
-                  elevation: 10,
                 ),
-                onPressed: () {},
-                child: Text(
-                  "등록",
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: Colors.white,
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      "0",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Expanded(
+                      child: Slider(
+                        value: ref.watch(pointSliderProvider),
+                        min: 0,
+                        max: 100,
+                        thumbColor: Colors.blue,
+                        activeColor: Colors.blue,
+                        onChanged: (newValue) {
+                          ref
+                              .read(pointSliderProvider.notifier)
+                              .setPoint(newValue);
+                        },
                       ),
+                    ),
+                    Text(
+                      "100",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(15),
+                    elevation: 10,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "등록",
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
