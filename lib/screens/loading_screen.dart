f0e713fc05357f459dc42ae3513b92c95c0d561c
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-10-07 16:30:19 +0900

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -5,6 +5,7 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: CircularProgressIndicator(),
       ),
