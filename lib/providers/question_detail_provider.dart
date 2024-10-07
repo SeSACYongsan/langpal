d006f13aefe6e152d43fdeabd683d1bb7e6d6020
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-10-07 16:09:10 +0900

    Update

---
+++ lib/providers/question_detail_provider.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:langpal/models/answer.dart';
+import 'package:langpal/models/langpal_user.dart';
+import 'package:langpal/models/question.dart';
+
+final questionDetailProvider = FutureProvider.family
+    .autoDispose<Map<String, dynamic>?, String>((ref, questionID) async {
+  final firestoreInstance = FirebaseFirestore.instance;
+  final questionReference =
+      firestoreInstance.collection("questions").doc(questionID);
+  final questionSnapshot = await questionReference.get();
+  try {
+    if (questionSnapshot.exists) {
+      if (questionSnapshot.data() != null) {
+        final question = Question.fromMap(questionSnapshot.data()!);
+        final ownerID = question.ownerID;
+        final userReference =
+            firestoreInstance.collection("users").doc(ownerID);
+        final userSnapshot = await userReference.get();
+        if (userSnapshot.exists) {
+          if (userSnapshot.data() != null) {
+            final user = LangpalUser.fromMap(userSnapshot.data()!);
+            final answers = firestoreInstance.collection("answers");
+            final answersSnapshot =
+                await answers.where("questionID", isEqualTo: questionID).get();
+            final matchingAnswers = answersSnapshot.docs.map((document) {
+              if (document.exists) {
+                return Answer.fromMap(document.data());
+              } else {
+                throw Exception("The answer document doesn't exist");
+              }
+            }).toList();
+            matchingAnswers.sort((a, b) {
+              return a.date.compareTo(b.date);
+            });
+            return {
+              "question": question,
+              "user": user,
+              "answers": matchingAnswers
+            };
+          } else {
+            throw Exception("The user doesn't exist");
+          }
+        } else {
+          throw Exception("The user snapshot doesn't exist");
+        }
+      } else {
+        throw Exception("The question is null");
+      }
+    } else {
+      throw Exception("The question snapshot doesn't exist");
+    }
+  } catch (error) {
+    print(error);
+  }
+  return null;
+});
--- lib/screens/question_detail_screen.dart
+++ lib/screens/question_detail_screen.dart
@@ -2,13 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:langpal/models/answer.dart';
+import 'package:langpal/models/langpal_user.dart';
 import 'package:langpal/models/language.dart';
 import 'package:langpal/models/level.dart';
-import 'package:langpal/providers/answers_provider.dart';
+import 'package:langpal/models/question.dart';
 import 'package:langpal/providers/current_user_id_provider.dart';
 import 'package:langpal/providers/fields/answer_text_field_provider.dart';
-import 'package:langpal/providers/question_provider.dart';
-import 'package:langpal/providers/user_provider.dart';
+import 'package:langpal/providers/question_detail_provider.dart';
 import 'package:langpal/screens/error_screen.dart';
 import 'package:langpal/screens/loading_screen.dart';
 import 'package:uuid/uuid.dart';
@@ -25,221 +25,185 @@ class _QuestionDetailScreenState extends ConsumerState<QuestionDetailScreen> {
   late TextEditingController answerTextEditingController;
   @override
   Widget build(BuildContext context) {
-    final asyncQuestion = ref.watch(questionProvider);
-    final asyncUser = ref.watch(userProvider);
-    final asyncAnswers = ref.watch(answersProvider);
+    final asyncData = ref.watch(questionDetailProvider(widget.questionID));
     answerTextEditingController.text = ref.watch(answerTextFieldProvider);
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text(
-          "답변하기",
-          style: TextStyle(color: Colors.white),
-        ),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back_ios_new,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
-      body: asyncQuestion.when(data: (question) {
-        if (question == null) {
+    return asyncData.when(
+      error: (error, stackTrace) {
+        return ErrorScreen(message: error.toString());
+      },
+      loading: () {
+        return const LoadingScreen();
+      },
+      data: (data) {
+        if (data == null) {
           return const LoadingScreen();
         } else {
-          final userID = question.ownerID;
-          ref.read(userProvider.notifier).getUserByID(userID);
-          return asyncUser.when(
-            data: (user) {
-              if (user == null) {
-                return const LoadingScreen();
-              } else {
-                return asyncAnswers.when(
-                  data: (answers) {
-                    return SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Column(
+          final question = data["question"] as Question;
+          final user = data["user"] as LangpalUser;
+          final answers = data["answers"] as List<Answer>;
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              backgroundColor: Colors.blue,
+              title: const Text(
+                "답변하기",
+                style: TextStyle(color: Colors.white),
+              ),
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back_ios_new,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.asset(
+                            "assets/images/profile.png",
+                            width: 70,
+                            errorBuilder: (context, error, stackTrace) {
+                              return const Icon(Icons.person);
+                            },
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(50),
-                                  child: Image.asset(
-                                    "assets/images/profile.png",
-                                    width: 70,
-                                    errorBuilder: (context, error, stackTrace) {
-                                      return const Icon(Icons.person);
-                                    },
-                                  ),
-                                ),
-                                const SizedBox(width: 20),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      user.info.username,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleLarge,
-                                    ),
-                                    Text(
-                                      "${user.info.targetLanguage.toKoreanName()} 수준: ${user.info.level.toKoreanName()}",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleMedium,
-                                    ),
-                                  ],
-                                ),
-                              ],
+                            Text(
+                              user.info.username,
+                              style: Theme.of(context).textTheme.titleLarge,
                             ),
-                            ...answers.map((answer) {
-                              return Text(answer.content);
-                            }),
-                            const SizedBox(height: 20),
-                            Container(
-                              padding: const EdgeInsets.all(20),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    blurRadius: 5,
-                                    spreadRadius: 5,
-                                    offset: Offset(5, 5),
-                                    color: Colors.black12,
-                                  ),
-                                ],
-                              ),
-                              child: Text(
-                                question.content,
-                                style: Theme.of(context).textTheme.titleSmall,
-                              ),
-                            ),
-                            const SizedBox(height: 30),
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(50),
-                              child: Image.asset(
-                                "assets/images/profile.png",
-                                width: 70,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return const Icon(Icons.person);
-                                },
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    blurRadius: 5,
-                                    spreadRadius: 5,
-                                    offset: Offset(5, 5),
-                                    color: Colors.black12,
-                                  ),
-                                ],
-                              ),
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 10),
-                                child: TextField(
-                                  controller: answerTextEditingController,
-                                  keyboardType: TextInputType.text,
-                                  decoration: const InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "답변을 입력하세요",
-                                  ),
-                                  maxLines: 8,
-                                  onChanged: (value) {
-                                    ref
-                                        .read(answerTextFieldProvider.notifier)
-                                        .setAnswer(value);
-                                  },
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            SizedBox(
-                              width: double.infinity,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  padding: const EdgeInsets.all(15),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  elevation: 10,
-                                  backgroundColor: Colors.blue,
-                                  foregroundColor: Colors.white,
-                                ),
-                                onPressed: () {
-                                  if (answerTextEditingController.text
-                                      .trim()
-                                      .isEmpty) {
-                                    showDialog(
-                                      context: context,
-                                      builder: (context) {
-                                        return const AlertDialog(
-                                          title: Text("내용을 입력해주세요"),
-                                          icon: Icon(Icons.error),
-                                        );
-                                      },
-                                    );
-                                  } else {
-                                    submitAnswer();
-                                  }
-                                },
-                                child: Text(
-                                  "답변하기",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .titleLarge!
-                                      .copyWith(
-                                        color: Colors.white,
-                                      ),
-                                ),
-                              ),
+                            Text(
+                              "${user.info.targetLanguage.toKoreanName()} 수준: ${user.info.level.toKoreanName()}",
+                              style: Theme.of(context).textTheme.titleMedium,
                             ),
                           ],
                         ),
+                      ],
+                    ),
+                    ...answers.map((answer) {
+                      return Text(answer.content);
+                    }),
+                    const SizedBox(height: 20),
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                            offset: Offset(5, 5),
+                            color: Colors.black12,
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                  error: (error, stackTrace) {
-                    return ErrorScreen(
-                      message: error.toString(),
-                    );
-                  },
-                  loading: () {
-                    return const LoadingScreen();
-                  },
-                );
-              }
-            },
-            error: (error, stackTrace) {
-              return ErrorScreen(
-                message: error.toString(),
-              );
-            },
-            loading: () {
-              return const LoadingScreen();
-            },
+                      child: Text(
+                        question.content,
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.asset(
+                        "assets/images/profile.png",
+                        width: 70,
+                        errorBuilder: (context, error, stackTrace) {
+                          return const Icon(Icons.person);
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                            offset: Offset(5, 5),
+                            color: Colors.black12,
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: TextField(
+                          controller: answerTextEditingController,
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "답변을 입력하세요",
+                          ),
+                          maxLines: 8,
+                          onChanged: (value) {
+                            ref
+                                .read(answerTextFieldProvider.notifier)
+                                .setAnswer(value);
+                          },
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          elevation: 10,
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          if (answerTextEditingController.text.trim().isEmpty) {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return const AlertDialog(
+                                  title: Text("내용을 입력해주세요"),
+                                  icon: Icon(Icons.error),
+                                );
+                              },
+                            );
+                          } else {
+                            submitAnswer();
+                          }
+                        },
+                        child: Text(
+                          "답변하기",
+                          style:
+                              Theme.of(context).textTheme.titleLarge!.copyWith(
+                                    color: Colors.white,
+                                  ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           );
         }
-      }, error: (error, stackTrace) {
-        return ErrorScreen(
-          message: error.toString(),
-        );
-      }, loading: () {
-        return const LoadingScreen();
-      }),
+      },
     );
   }
 
@@ -252,10 +216,6 @@ class _QuestionDetailScreenState extends ConsumerState<QuestionDetailScreen> {
   @override
   void initState() {
     answerTextEditingController = TextEditingController();
-    ref.read(questionProvider.notifier).getQuestionByID(widget.questionID);
-    ref
-        .read(answersProvider.notifier)
-        .getAnswersByQuestionID(widget.questionID);
     super.initState();
   }
 
@@ -264,6 +224,7 @@ class _QuestionDetailScreenState extends ConsumerState<QuestionDetailScreen> {
     final answerReference =
         firestoreInstance.collection("answers").doc(answer.id);
     await answerReference.set(answer.toMap());
+    ref.refresh(questionDetailProvider(widget.questionID));
   }
 
   Future<void> submitAnswer() async {
