ac37137575cb57093b9001abcbb0c6051b96954a
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-10-08 08:32:25 +0900

    Update

---
+++ lib/providers/my_answers_provider.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:langpal/models/answer.dart';
+import 'package:langpal/models/langpal_user.dart';
+import 'package:langpal/providers/current_user_id_provider.dart';
+
+final myAnswersProvider = FutureProvider<Map<String, dynamic>?>((ref) async {
+  final currentUserID = ref.read(currentUserIDProvider);
+  final firestoreInstance = FirebaseFirestore.instance;
+  final answers = await firestoreInstance
+      .collection("answers")
+      .where("ownerID", isEqualTo: currentUserID)
+      .get();
+  try {
+    final matchingAnswers = answers.docs.map((document) {
+      if (document.exists) {
+        return Answer.fromMap(document.data());
+      } else {
+        throw Exception("The answer document doesn't exist");
+      }
+    }).toList();
+    final userSnapshot =
+        await firestoreInstance.collection("users").doc(currentUserID).get();
+    if (userSnapshot.exists) {
+      final user = LangpalUser.fromMap(userSnapshot.data()!);
+      return {
+        "answers": matchingAnswers,
+        "user": user,
+      };
+    } else {
+      throw Exception("The user snapshot doesn't exist");
+    }
+  } catch (error) {
+    print(error);
+  }
+  return null;
+});
--- lib/screens/my_answers_screen.dart
+++ lib/screens/my_answers_screen.dart
@@ -1,75 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:langpal/components/answer_card.dart';
+import 'package:langpal/models/answer.dart';
+import 'package:langpal/models/langpal_user.dart';
+import 'package:langpal/providers/my_answers_provider.dart';
+import 'package:langpal/screens/error_screen.dart';
+import 'package:langpal/screens/loading_screen.dart';
 
-class MyAnswersScreen extends StatelessWidget {
+class MyAnswersScreen extends ConsumerWidget {
   const MyAnswersScreen({super.key});
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text(
-          "나의 답변",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back_ios_new,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        backgroundColor: Colors.blue,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  context.go("/main/my_page/my_answers/detail/wow");
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        offset: Offset(5, 5),
-                        color: Colors.black12,
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        "2024-07-31 15:14",
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ),
-                      Text(
-                        "\"안녕하시요\"",
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      Text(
-                        "무슨 뜻인가요?",
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                    ],
-                  ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncAnswers = ref.watch(myAnswersProvider);
+    return asyncAnswers.when(
+      error: (error, stackTrace) {
+        return ErrorScreen(message: error.toString());
+      },
+      loading: () {
+        return const LoadingScreen();
+      },
+      data: (data) {
+        if (data == null) {
+          return const LoadingScreen();
+        } else {
+          final user = data["user"] as LangpalUser;
+          final answers = data["answers"] as List<Answer>;
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              title: const Text(
+                "나의 답변",
+                style: TextStyle(
+                  color: Colors.white,
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back_ios_new,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              backgroundColor: Colors.blue,
+            ),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ...answers.map(
+                      (answer) {
+                        return GestureDetector(
+                          onTap: () {
+                            context.go(
+                                "/main/my_page/my_answers/detail/${answer.id}");
+                          },
+                          child: AnswerCard(
+                            answer: answer,
+                            user: user,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
