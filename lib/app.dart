ffb37cbe0fdfa1ffaf13741d3a1b867c703a3824
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-10-14 18:31:36 +0900

    Update

--- lib/app.dart
+++ lib/app.dart
@@ -4,11 +4,10 @@ import 'package:langpal/providers/router/router_provider.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-      routerConfig: ref.read(routerProvider),
+      routerConfig: router,
       title: "Langpal",
       theme: ThemeData(
         fontFamily: "CookieRun",
